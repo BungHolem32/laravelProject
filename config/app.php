b3fa4ea43b427ecbd7af7bd9057c5a0ef5676053
ilanvac <ilanvac@gmail.com>
2016-06-20 07:52:28 +0300

    create login page in crm

--- config/app.php
+++ config/app.php
@@ -154,11 +154,11 @@ return [
          * Application Service Providers...
          */
         App\Providers\AppServiceProvider::class,
+
         App\Providers\AuthServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         Collective\Html\HtmlServiceProvider::class,
-        App\Providers\DBMod::class
 
 
     ],
